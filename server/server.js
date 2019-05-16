6ba62df428e99674fcd30aa96436e64ea1885a1e
Alex <alex@flindel.com>
2019-05-16 01:04:03 -0400

    Change to clearer structure

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ dotenv.config();
 
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
+const app = next({ 
+  dev,
+  dir: './client'
+});
 const handle = app.getRequestHandler();
 
 const router = require('./routes/index');
