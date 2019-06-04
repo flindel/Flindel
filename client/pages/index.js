6c6a6e4bb327004e76079bb0090a6414184d7822
yash-kini <yash.1@hotmail.com>
2019-06-04 15:07:21 -0400

    Revert "Updated Plugin-App"
    This reverts commit 775b84d1fc73861b32d3d33cf59b38a8efb9b313.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import IdentifyApp from "./Confirmation/IdentifyApp2";
+import { BrowserRouter, Route } from 'react-router-dom';
+import IdentifyApp from "./IdentifyItems/IdentifyApp";
 
 class Index extends React.Component{
   render(){
     return (
     <div>
       <div>
+          <p>Sample app using React and Next.js</p>
           <IdentifyApp />
       </div>
     </div>
