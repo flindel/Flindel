2816c8d29be696c5298304f8c289dabb0e64d109
yash-kini <yash.1@hotmail.com>
2019-06-04 15:06:54 -0400

    Revert "Fixing over-write"
    This reverts commit 242e95b2f4825127da1c8587ffb7b6ec601a77e9.

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,15 +2,11 @@ const combineRouters = require('koa-combine-routers');
 const rootRouter = require('./root');
 const orderRouter = require('./orders');
 const productRouter = require('./products');
-//const sendRouter = require('./sendEmail')
-const checkProductRouter = require('./checkProducts')
 
 router = combineRouters(
     rootRouter,
     orderRouter,
-    productRouter,
-    //sendRouter
-    checkProductRouter
+    productRouter
 )
 
 module.exports = router;
