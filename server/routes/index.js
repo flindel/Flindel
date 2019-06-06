63f77346ba82581f36c82e8d21e3a63615368852
Mike McColm <mike.mccolm28@gmail.com>
2019-06-06 12:15:29 -0400

    Combining routers

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,12 @@ const rootRouter = require('./root');
 const orderRouter = require('./orders');
 const productRouter = require('./products');
 const sendRouter = require('./sendEmail')
-const checkProductRouter = require('./checkProducts')
 
 router = combineRouters(
     rootRouter,
     orderRouter,
     productRouter,
     sendRouter,
-    checkProductRouter
 )
 
 module.exports = router;
--- server/routes/products.js
+++ server/routes/products.js
@@ -7,6 +7,38 @@ const router = Router({
     prefix: '/products'
 });
 
+router.get('/', async ctx => {
+    // Get product img src
+
+    const productid = ctx.query.id;
+    console.log("productID:---------"+productid)
+    const { cookies } = ctx;
+    const shop = cookies.get('shop_id');
+    const accessToken = cookies.get('accessToken');
+    const option = {
+        //`https://getordertest.myshopify.com/admin/api/2019-04/products/${this.props.item.productID}.json?fields=image`
+        url: `https://${shop}/${api_link}/products/${productid}.json`,
+        headers: {
+            'X-Shopify-Access-Token': accessToken
+        },
+        json: true,
+    }
+
+    try {
+        ctx.body = await rp(option);
+        //console.log("body..."+JSON.stringify(ctx.body));
+    } catch (err) {
+        console.log(err.message);
+        if (err instanceof errors.StatusCodeError) {
+            ctx.status = err.statusCode;
+            ctx.message = err.message;
+        } else if (err instanceof errors.RequestError) {
+            ctx.status = 500;
+            ctx.message = err.message;
+        }
+    }
+    });
+
 router.post('/', async ctx => {
     // Create a product
     const { shop, accessToken } = getShopHeaders(ctx);
