ddf16cb2983d44cf253b553e377574d8e629f9b2
Cengiz Sirlan <cengizsirlan.cs@gmail.com>
2019-08-01 12:32:10 -0400

    Canceling order works and theme webhook added

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -7,32 +7,29 @@ const router = Router({
   prefix: "/orders"
 });
 
-router.get('/', async ctx => {
-    // Get all orders
-    const { shop, accessToken } = getShopHeaders(ctx);
-    const name = ctx.query.orderNum;
-    const { cookies } = ctx;
-    const option = {
-        url: `https://${shop}/${api_link}/orders.json?name=${name}&status=any`,
-        headers: {
-            'X-Shopify-Access-Token': accessToken
-        },
-        json: true,
-    }
-    try {
-        ctx.body = await rp(option);
-    } catch (err) {
-        console.log(err.message);
-        if (err instanceof errors.StatusCodeError) {
-            ctx.status = err.statusCode;
-            ctx.message = err.message;
-        } else if (err instanceof errors.RequestError) {
-            ctx.status = 500;
-            ctx.message = err.message;
-        }
+router.get("/", async ctx => {
+  // Get all orders
+  const { shop, accessToken } = getShopHeaders(ctx);
+  const option = {
+    url: `https://${shop}/${api_link}/orders.json`,
+    headers: {
+      "X-Shopify-Access-Token": accessToken
+    },
+    json: true
+  };
+  try {
+    ctx.body = await rp(option);
+  } catch (err) {
+    console.log(err.message);
+    if (err instanceof errors.StatusCodeError) {
+      ctx.status = err.statusCode;
+      ctx.message = err.message;
+    } else if (err instanceof errors.RequestError) {
+      ctx.status = 500;
+      ctx.message = err.message;
     }
   }
-);
+});
 
 //cancel order
 router.post("/cancel", async ctx => {
@@ -44,18 +41,18 @@ router.post("/cancel", async ctx => {
   } else {
     headers["X-Shopify-Access-Token"] = accessToken;
   }
+
   const option = {
     method: "POST",
-    url: `https://${shop}/${api_link}/orders/#${order_id}/cancel.json`,
+    url: `https://databasecommunicationtest.myshopify.com/${api_link}/orders/${order_id}/cancel.json`,
     headers: headers,
     json: true
   };
   try {
+    //console.log("what DDDDDDDDDDD: ", option.url);
     ctx.body = await rp(option);
-
     console.log("CANCEL " + JSON.stringify(ctx.body));
   } catch (err) {
-    console.log(err.message);
     if (err instanceof errors.StatusCodeError) {
       ctx.status = err.statusCode;
       ctx.message = err.message;
