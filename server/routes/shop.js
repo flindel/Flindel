fc837a077bb9995f463b1bd66dfcbd4e8389f3cd
ludiw823@gmail.com <ludiw823@gmail.com>
2019-08-16 16:12:16 -0400

    Update shop query for return portal

--- server/routes/shop.js
+++ server/routes/shop.js
@@ -13,46 +13,42 @@ router.get('/id/', async ctx => {
   ctx.body = JSON.stringify({shop_id: shop});
 });
 
-router.get('/initialization', async ctx => {
-    const shop = ctx.query.shop
-    console.log(shop)
-    ctx.cookies.set('shop_id',shop)
-    ctx.redirect(`https://${ctx.headers.host}`)
-    ctx.body = {'initilization': shop}
-})
-
 //get shop domain 
 router.get('/domain', async ctx =>{
   const { cookies } = ctx;
   const shop = cookies.get('shop_id');
-  const accessToken = cookies.get('accessToken');
-  const option = {
-      method: 'GET',
-      url: `https://${shop}/${api_link}/shop.json`,
-      headers: {
-        'X-Shopify-Access-Token': accessToken
-      },
-      json: true,
-  }
-  try {
-      let res = await rp(option);
-      ctx.body = {'domain':res.shop.domain}
-      console.log("get shop domain body..."+JSON.stringify(ctx.body));
-  } catch (err) {
-      console.log(err.message);
-      if (err instanceof errors.StatusCodeError) {
-          ctx.status = err.statusCode;
-          ctx.message = err.message;
-      } else if (err instanceof errors.RequestError) {
-          ctx.status = 500;
-          ctx.message = err.message;
-      }
-  }
+  ctx.body = {'domain':shop}
+  console.log("get shop domain body..."+JSON.stringify(ctx.body));
+  //const accessToken = cookies.get('accessToken');
+  // const option = {
+  //     method: 'GET',
+  //     url: `https://${shop}/${api_link}/shop.json`,
+  //     headers: {
+  //       'X-Shopify-Access-Token': accessToken
+  //     },
+  //     json: true,
+  // }
+  // try {
+  //     let res = await rp(option);
+  //     ctx.body = {'domain':res.shop.domain}
+  //     console.log("get shop domain body..."+JSON.stringify(ctx.body));
+  // } catch (err) {
+  //     console.log(err.message);
+  //     if (err instanceof errors.StatusCodeError) {
+  //         ctx.status = err.statusCode;
+  //         ctx.message = err.message;
+  //     } else if (err instanceof errors.RequestError) {
+  //         ctx.status = 500;
+  //         ctx.message = err.message;
+  //     }
+  // }
 })
 
 router.get('/returnPolicy', async ctx =>{
     console.log(ctx)
     const { shop, accessToken } = getShopHeaders(ctx);
+    //const { cookies } = ctx;
+    //const shop = cookies.get('shop_id');
     db = ctx.db
     myRef = db.collection('store')
     let query = await myRef.doc(shop).get()
