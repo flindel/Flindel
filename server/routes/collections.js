753e65305d6bf6245ba1d12054a62ca59cce232a
yash-kini <yash.1@hotmail.com>
2019-08-29 17:40:36 -0400

    Update collections.js

--- server/routes/collections.js
+++ server/routes/collections.js
@@ -11,13 +11,40 @@ const router = Router({
 router.get("/", async ctx => {
   //Gets products all products from a collection
   const collectionid = ctx.query.id;
-  const page = ctx.query.page;
   console.log("collectionid:---------", collectionid);
   const { cookies } = ctx;
   const shop = cookies.get("shop_id");
   const accessToken = cookies.get("accessToken");
   const option = {
     url: `https://${shop}/${api_link}/products.json?collection_id=${collectionid}&limit=250&page=${page}`,
+    headers: {
+      "X-Shopify-Access-Token": accessToken
+    },
+    json: true
+  };
+  try {
+    ctx.body = await rp(option);
+    //console.log("body..."+JSON.stringify(ctx.body));
+  } catch (err) {
+    console.log(err.message);
+    if (err instanceof errors.StatusCodeError) {
+      ctx.status = err.statusCode;
+      ctx.message = err.message;
+    } else if (err instanceof errors.RequestError) {
+      ctx.status = 500;
+      ctx.message = err.message;
+    }
+  }
+});
+
+router.get("/page/", async ctx => {
+  //Gets products all products from a collection
+  const page_info = ctx.query.page_info;
+  const { cookies } = ctx;
+  const shop = cookies.get("shop_id");
+  const accessToken = cookies.get("accessToken");
+  const option = {
+    url: `https://${shop}/${api_link}/products.json?page_info=${page_info}&limit=250`,
     headers: {
       "X-Shopify-Access-Token": accessToken
     },
